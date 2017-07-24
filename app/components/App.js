df87c6261be0dd46642bc5651590c4095739b78b
mohamed <toot2009@hotmail.com>
2017-07-24 17:20:59 +0300

    add/Routing and the Main components

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+// import ReactRouter from 'react-router-dom';
+// Route Dependencies
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {Switch} from 'react-router-dom';
+//components rendering
+import Nav from './Nav';
+import Home from './Home';
 import Popular from './Popular';
+import Battle from './Battle';
+
+
 class App extends React.Component {
   render(){
     return (
-        <div className='container'>
-        <Popular /> 
-        </div>
+    	<Router>
+     	   <div className='container'>
+     	   	<Nav />
+          <Switch>
+      		 <Route exact path= '/' component= {Home}/>
+      		 <Route exact path= '/battle' component= {Battle}/>
+      		 <Route path= '/popular' component= {Popular}/>
+           <Route render= {()=>{
+            return <p>Not Found</p>
+           }} />
+           </Switch>
+       	    </div>
+    	</Router>
       )
   }
 }
