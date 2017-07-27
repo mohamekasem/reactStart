0f239fa37ac41bebf4df2e4a78ea47b1ab643903
mohamed <toot2009@hotmail.com>
2017-07-27 19:26:53 +0300

    update/api file and add some functions

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var App = require('./components/App')
-require('./index.css');
+import React from 'react';
+import App from'./components/App';
+import './index.css';
 
 ReactDOM.render(
 <App />,
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,13 +1,66 @@
 //axios it's promise http client 
 import axios from 'axios';
 
+const getProfile = (username)=>{
+	return axios.get('https://api.github.com/users/' + username)
+		.then((user)=> user.data
+	)
+}
+
+const getRepos = (username)=>{
+	return axios.get('https://api.github.com/users/'+ username + '/repos')
+
+}
+
+const getStarCount = (repos)=>{
+	return repos.data.reduce((count, repo)=> count + repo.stargazers_count, 0)
+}
+
+const claculateScore = (profile, repos)=>{
+	let followers = profile.followers;
+	let totalStars = getStarCount(repos);
+
+	return (followers * 3)+ totalStars;
+}
+
+const handleError = (error)=> {
+	console.warn(error);
+	return null;
+}
+
+const sortPlayers = (players)=>{
+	return players.sort((a,b)=> b.score - a.score);
+}
+
+function getUserData(player) {
+	return axios.all([
+			getProfile(player),
+			getRepos(player)])
+		.then((data)=>{
+			let profile = data[0];
+			let repos = data[1];
+
+			return {
+				profile: profile,
+				score: claculateScore(profile, repos)
+			}
+		})
+}
 module.exports = {
-	fetchPopularRepos : (language)=> { 
-		let encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
-		return axios.get(encodedURI)
+
+battle: (players)=>{
+	return axios.all(players.map(getUserData))
+		.then(sortPlayers)
+		.catch(handleError)	
+},
+
+fetchPopularRepos : (language)=> { 
+	let encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
+	return axios.get(encodedURI)
 		.then((res)=>{
 			return res.data.items;
-		});
-	}
+		}
+	);
+}
 }
 
